2d4684001be9f49d951af51b8ce0d86a6d137bf5
David Puentes <pipe01112009@gmail.com>
2026-03-24 18:40:54 -0500

    Finishing class #135.

--- src/05-useReducer/reducers/taskReducer.ts
+++ src/05-useReducer/reducers/taskReducer.ts
@@ -5,7 +5,7 @@ interface ToDo {
 }
 
 interface TaskState {
-  toDos: TaskState[];
+  toDos: ToDo[];
   toDosLength: number;
   completedToDosQuantity: number;
   pendingToDosQuantity: number;
@@ -13,12 +13,47 @@ interface TaskState {
 
 export type TaskAction =
   | { type: "ADD_TODO"; payload: string }
-  | { type: "TOGGLE_TODO"; payload: number }
-  | { type: "DELETE_TODO"; payload: number };
+  | { type: "DELETE_TODO"; payload: number }
+  | { type: "TOGGLE_TODO"; payload: number };
 
 export const taskReducer = function (
   state: TaskState,
   action: TaskAction,
 ): TaskState {
-  return {};
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newToDoText = action.payload.trim();
+      if (!newToDoText) return state;
+
+      const newToDo: ToDo = {
+        id: Date.now(),
+        text: newToDoText,
+        completed: false,
+      };
+
+      return { ...state, toDos: [newToDo, ...state.toDos] };
+    }
+
+    case "DELETE_TODO":
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+      };
+
+    case "TOGGLE_TODO": {
+      const updatedToDos = state.toDos.map((toDo) => {
+        if (toDo.id == action.payload) {
+          const editedToDo = toDo;
+          editedToDo.completed = !toDo.completed;
+          return editedToDo;
+        }
+        return toDo;
+      });
+
+      return { ...state, toDos: updatedToDos };
+    }
+
+    default:
+      return state;
+  }
 };
