bfb2b6e1c45fe51a27b5f574cd3efe11840a72e3
David Puentes <pipe01112009@gmail.com>
2026-03-31 15:48:57 -0500

    Finishing homework from class #167.

--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,13 +1,23 @@
+import { UserContext } from "@/09-useContext/context/userContext";
 import { Button } from "@/components/ui/button";
+import { useContext } from "react";
+import { Link } from "react-router";
 
 export const ProfilePage = function () {
+  const { user } = useContext(UserContext);
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-col items-center justify-center gap-4 w-full">
       <h1 className="h1">Mi perfíl</h1>
 
-      <pre>{JSON.stringify({}, null, 2)}</pre>
-
-      <Button variant="destructive">Salir</Button>
+      <div className="flex max-w-full max-h-[60vh] px-10 py-5 bg-black rounded-4xl overflow-hidden">
+        <pre className="overflow-scroll">
+          <code>{JSON.stringify(user, null, 2)}</code>
+        </pre>
+      </div>
+      <Link to={"/"}>
+        <Button variant="destructive">Salir</Button>
+      </Link>
     </div>
   );
 };
